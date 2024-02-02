605daa3714f92fe80f319791299a900321f34fbb
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-02-02 10:50:38 -0600

    Drop R support from base WT image. Fallback to plan PythonBuildPack

--- repo2docker_wholetale/jupyter.py
+++ repo2docker_wholetale/jupyter.py
@@ -2,41 +2,23 @@
 
 """Main module."""
 
-import json
 import os
 
-from .wholetale import WholeTaleRBuildPack
+from .wholetale import WholeTaleBuildPack
 
 
-class JupyterWTStackBuildPack(WholeTaleRBuildPack):
-
-    @property
-    def wt_env(self):
-        if self._wt_env is None:
-            with open(self.binder_path("environment.json"), "r") as fp:
-                env = json.load(fp)
-            self._wt_env = dict([_.split("=") for _ in env["config"]["environment"]])
-        return self._wt_env
-
+class JupyterWTStackBuildPack(WholeTaleBuildPack):
     def detect(self, buildpack="PythonBuildPack"):
         return super().detect(buildpack=buildpack)
 
     def get_build_script_files(self):
-        """Dict of files to be copied to the container image for use in building
-        """
+        """Dict of files to be copied to the container image for use in building"""
         files = {}
         for k, v in {
             "base/healthcheck.py": "/healthcheck.py",
-            "base/jupyter_notebook_config.py": "${HOME}/.jupyter/jupyter_notebook_config.py"
+            "base/jupyter_notebook_config.py": "${HOME}/.jupyter/jupyter_notebook_config.py",
         }.items():
             files[os.path.join(os.path.dirname(__file__), k)] = v
 
         files.update(super().get_build_script_files())
         return files
-
-    def get_build_scripts(self):
-        scripts = [
-            ("root", r"""mkdir ${HOME}/work"""),
-            ("root", r"""chown -R ${NB_USER}:${NB_USER} ${HOME}"""),
-        ]
-        return super().get_build_scripts() + scripts
--- repo2docker_wholetale/wholetale.py
+++ repo2docker_wholetale/wholetale.py
@@ -4,23 +4,20 @@
 
 import datetime
 import json
-import re
 import os
-import shutil
-import tempfile
 
-from repo2docker.buildpacks.base import BuildPack
 from repo2docker.buildpacks.r import RBuildPack
 from repo2docker.buildpacks.python import PythonBuildPack
 
 
-class WholeTaleBuildPack(BuildPack):
+class WholeTaleBuildPack(PythonBuildPack):
 
-    major_pythons = {"2": "2.7", "3": "3.8"}
+    major_pythons = {"2": "2.7", "3": "3.10"}
     _wt_env = None
 
-    def get_build_args(self):
-        return {}
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.base_image = "docker.io/library/buildpack-deps:jammy"
 
     def binder_path(self, path):
         """
@@ -54,54 +51,17 @@ class WholeTaleBuildPack(BuildPack):
         )
         return files
 
-    def apt_assemble_script(self):
-        if os.path.exists(self.binder_path("apt.txt")):
-            with open(self.binder_path("apt.txt")) as f:
-                apt_packages = []
-                for l in f:
-                    package = l.partition("#")[0].strip()
-                    if not package:
-                        continue
-                    # Validate that this is, indeed, just a list of packages
-                    # We're doing shell injection around here, gotta be careful.
-                    # FIXME: Add support for specifying version numbers
-                    if not re.match(r"^[a-z0-9.+-]+", package):
-                        raise ValueError(
-                            "Found invalid package name {} in "
-                            "apt.txt".format(package)
-                        )
-                    apt_packages.append(package)
+    def detect(self, buildpack=None):
+        if not os.path.exists(self.binder_path("environment.json")):
+            return False
 
-            if apt_packages:
-                return (
-                    "root",
-                    # This apt-get install is *not* quiet, since users explicitly asked for this
-                    r"""
-                    apt-get -qq update && \
-                    apt-get install --yes --no-install-recommends {} && \
-                    apt-get -qq purge && \
-                    apt-get -qq clean && \
-                    rm -rf /var/lib/apt/lists/*
-                    """.format(
-                        " ".join(apt_packages)
-                    ),
-                )
+        with open(self.binder_path("environment.json"), "r") as fp:
+            env = json.load(fp)
 
-    def installR_assemble_script(self):
-        installR_path = self.binder_path("install.R")
-        if os.path.exists(installR_path):
-            return ("${NB_USER}", "Rscript %s" % installR_path)
-
-    def get_post_build_scripts(self):
-        post_build = self.binder_path("postBuild")
-        if os.path.exists(post_build):
-            return [post_build]
-        return []
-
-    def descriptionR_assemble_script(self):
-        description_R = "DESCRIPTION"
-        if not self.binder_dir and os.path.exists(description_R):
-            return ("${NB_USER}", 'R --quiet -e "devtools::install_local(getwd())"')
+        try:
+            return env["config"]["buildpack"] == buildpack
+        except (KeyError, TypeError):
+            return False
 
     @property
     def wt_env(self):
@@ -111,42 +71,10 @@ class WholeTaleBuildPack(BuildPack):
             self._wt_env = dict([_.split("=") for _ in env["config"]["environment"]])
         return self._wt_env
 
-    def _build(self, *args, **kwargs):
-        """Not used right now...."""
-        tempdir = tempfile.mkdtemp()
-
-        with open(os.path.join(tempdir, "Dockerfile"), "wb") as f:
-            f.write(self.render().encode("utf-8"))
-
-        shutil.copytree(".", os.path.join(tempdir, "src"))
-
-        args[-1].update({"path": tempdir})  # extra_build_kwargs
-        yield from super().build(*args, **kwargs)
-
-        shutil.rmtree(tempdir, ignore_errors=True)
-
-    def build(
-        self,
-        client,
-        image_spec,
-        memory_limit,
-        build_args,
-        cache_from,
-        extra_build_kwargs,
-    ):
-        if build_args:
-            for k, v in self.get_build_args().items():
-                build_args.setdefault(k, v)
-        else:
-            build_args = self.get_build_args()
-        yield from super().build(
-            client, image_spec, memory_limit, build_args, cache_from, extra_build_kwargs
-        )
-
 
 class WholeTaleRBuildPack(RBuildPack):
 
-    major_pythons = {"2": "2.7", "3": "3.8"}
+    major_pythons = {"2": "2.7", "3": "3.10"}
 
     def binder_path(self, path):
         """
@@ -158,9 +86,6 @@ class WholeTaleRBuildPack(RBuildPack):
             if os.path.exists(possible_config_dir):
                 return os.path.join(possible_config_dir, path)
         return path
-
-    def get_build_args(self):
-        return {}
 
     @property
     def python_version(self):
@@ -191,21 +116,3 @@ class WholeTaleRBuildPack(RBuildPack):
                 return True
         except (KeyError, TypeError):
             return False
-
-    def build(
-        self,
-        client,
-        image_spec,
-        memory_limit,
-        build_args,
-        cache_from,
-        extra_build_kwargs,
-    ):
-        if build_args:
-            for k, v in self.get_build_args().items():
-                build_args.setdefault(k, v)
-        else:
-            build_args = self.get_build_args()
-        yield from super().build(
-            client, image_spec, memory_limit, build_args, cache_from, extra_build_kwargs
-        )
